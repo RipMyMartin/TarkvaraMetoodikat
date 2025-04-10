d72938c1d23418f04b7aa24acd02998bcaaa5f3d
Opilane TTHK <161806791+RipMyMartin@users.noreply.github.com>
2025-04-10 09:29:58 +0300

    streamlit

--- StreamlitTaskList.py
+++ StreamlitTaskList.py
@@ -1,40 +1,35 @@
 import streamlit as st
 
-def initSession():
-    if "tasks" not in st.sessionState:
-        st.sessionState.tasks = []
+st.session_state.tasks = []
+st.title("Task List App")
+def add_task():
+    task = st.text_input("Enter a new task:", key="new_task_input")
+    if st.button("Add Task"):
+        if task.strip():
+            st.session_state.tasks.append({"Text: ":task, "done" : False})
+            st.rerun()
+        else:
+            st.warning("Please enter a task!", icon="⚠️")
 
-def addTasks():
-    with st.form("add_task"):
-        newTaks = st.text_input("💡 Kirjutage uue Ylesanne")
-        submitted = st.form_submit_button("Lisada")
-        if submitted and newTaks:
-            st.sessionState.tasks.append(newTaks)
-            st.success("Ylesanne lisatud!")
 
-def showTasks():
-    if not st.sessionState.tasks:
-        st.info("Pole Ylesandeid, lisa ylesanne!")
+st.subheader("Task List: ")
+def show_tasks():
+    if not st.session_state.tasks:
+        st.info("No tasks added yet!")
         return
 
-    st.subheader("📋 Ylesannete list")
-    for i, task in enumerate(st.sessionState.tasks):
-        col1, col2 = st.columns([0.85, 0.15])
-        with col1:
-            st.write(f"{i + 1}. {task}")
-        with col2:
-            if st.button("❌", key=f"delete_{i}"):
-                st.sessionState.tasks.pop(i)
-                return deleteTask()
-            
-def deleteTask():
-    pass
-
-def main():
-    st.title("📝 Tasks list")
-    initSession()
-    addTasks()
-    showTasks()
+    for i, task in enumerate(st.session_state.tasks):
+        cols = st.columns([0.05, 0.80, 0.05])
+        with cols[0]:
+            task["done"] = st.checkbox("", value=task["done"], key=f"done_{i}")
+        with cols[1]:
+            text = task["Text: "]
+            st.markdown(text)
+        with cols[2]:
+            if st.button("Delete", key=f"delete_{i}"):
+                st.session_state.tasks.pop(i)
+                st.rerun()
 
 if __name__ == "__main__":
-    main()
+    add_task()
+    show_tasks()
