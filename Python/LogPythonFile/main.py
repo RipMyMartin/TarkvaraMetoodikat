948c298cb62bd3ce59d0ae464000a057b9cc5e34
ripmymartin <161806791+RipMyMartin@users.noreply.github.com>
2025-04-21 11:39:10 +0300

    full code

--- Python/LogPythonFile/main.py
+++ Python/LogPythonFile/main.py
@@ -1,48 +1,52 @@
-import json, datetime
+import json
+import datetime
 
 def validate_record(record):
     try:
         timestamp = record.get("timestamp")
 
         truck_id = record.get("truck_id")
-        if not truck_id or not isinstance(truck_id,str):
-            return False, "Invalid or missing truck_id \n", timestamp
+        if not truck_id or not isinstance(truck_id, str):
+            return False, "Invalid or missing truck_id", timestamp
 
         latitude = record.get("latitude")
-        if not isinstance(latitude,float):
-            return False, "Invalid latitude \n", timestamp
-
+        if not isinstance(latitude, float):
+            return False, "Invalid latitude", timestamp
 
         longitude = record.get("longitude")
-        if not isinstance(longitude,float):
-            return False, "Invalid longitude \n", timestamp
+        if not isinstance(longitude, float):
+            return False, "Invalid longitude", timestamp
 
         return True, "Valid", timestamp
 
     except Exception as e:
-        return False, f"Exeception: {str(e)} ", timestamp
+        return False, f"Exception: {str(e)}", None
 
 
-def procces_log_file(filepath):
+def process_log_file(filepath):
     records = []
-    with open(filepath,"r") as file:
-        records  = json.load(file)
-        print(records)
+    with open(filepath, "r") as file:
+        records = json.load(file)
+    
+    header = f"{'Truck ID':<20} {'Timestamp':<30} {'Status':<10} {'Message'}\n"
     
     with open("success.log", "w") as success_log, open("error.log", "w") as error_log:
+        success_log.write(header)
+        error_log.write(header)
+
         for record in records:
             valid, message, timestamp = validate_record(record)
-            print(valid, message, timestamp)
-
+            
+            if timestamp is None:
+                timestamp = "NoTime"
+            
             truck_id = record.get("truck_id", "UNKNOWN")
-            timestamp = record.get("timestamp", "NoTime")
-            line = f"{truck_id} | {timestamp}"
-
+            
+            line = f"{truck_id:<20} {timestamp:<30} "
 
             if valid:
-                success_log.write(line +  " | SUCCES \n")
+                success_log.write(line + f" {'SUCCESS':<10} {message}\n")
             else:
-                error_log.write(line + f" | ERROR: {message} \n")
+                error_log.write(line + f" {'ERROR':<10} {message}\n")
 
-
-procces_log_file("log_input.json")
+process_log_file("log_input.json")
