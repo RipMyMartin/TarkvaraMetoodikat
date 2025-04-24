3b5c37011c58a238bf62e63d947e0762398ebfa7
Opilane TTHK <161806791+RipMyMartin@users.noreply.github.com>
2025-04-24 11:42:24 +0300

    dsa

---
+++ Python/SundmusVali/main.py
@@ -0,0 +1,20 @@
+uudised = {
+    
+}
+
+while True:
+    print("Vali üks sünmus")
+
+    vali = int(input("Vali number 1-5:"))
+
+    if vali == 1:
+        print(f"Sa valisid {vali}")
+    elif vali == 2:
+        print(f"Sa valisid {vali}")
+    elif vali == 3:
+        print(f"Sa valisid {vali}")
+    elif vali == 4:
+        print(f"Sa valisid {vali}")
+    elif vali == 5:
+        print(f"Sa valisid {vali}")
+        
