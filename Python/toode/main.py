67dacd8cd101ba7292541b43a4c38250a9cce298
Opilane TTHK <opilane@tthk.ee>
2025-05-06 11:40:52 +0300

    asd

---
+++ Python/toode/main.py
@@ -0,0 +1,4 @@
+#Kasutajana soovin saada meeldetuletusi vee joomiseks, et ei unustaks joosule piisavalt vett juua.
+#Kasutajana soonin näha graafikud une, vee ja stressi kohta, et paremini aru saada oma harjumused
+#Kasutajana soovin teha eesmärke, et olla motiveeritud
+#Kasutajana soovin teha oma massi arvutamist ja kilokaloride arvutamist
